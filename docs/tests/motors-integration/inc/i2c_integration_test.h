dfd7317ac65f2262a8c17a6f7ca80bd72245a7f3
Hugo Lopes <hugofslopes@live.com.pt>
2026-02-03 16:39:30 +0000

    add motors integration tests code

---
+++ docs/tests/motors-integration/inc/i2c_integration_test.h
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file           : i2c_integration_test.h
+  * @brief          : Integration test header for I2C motor controllers
+  ******************************************************************************
+  */
+
+#ifndef __I2C_INTEGRATION_TEST_H
+#define __I2C_INTEGRATION_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "pca9685.h"
+
+
+/**
+ * @brief Run complete I2C integration test suite
+ * @return 0 if all tests pass, -1 if any test fails
+ */
+int32_t RunI2CIntegrationTests(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __I2C_INTEGRATION_TEST_H */
