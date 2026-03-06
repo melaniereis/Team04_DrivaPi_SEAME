0ce5db60f2940589658d9076c1e677779e090b46
Hugofslopes <hugo.lopes@seame.pt>
2026-03-06 12:13:05 +0000

    Update include guards in soft_i2c.h

--- firmware/Core/Inc/soft_i2c.h
+++ firmware/Core/Inc/soft_i2c.h
@@ -1,5 +1,5 @@
-#ifndef __INC_SOFT_I2C_H
-#define __INC_SOFT_I2C_H
+#ifndef __SOFT_I2C_H
+#define __SOFT_I2C_H
 
 #include "main.h"
 
@@ -8,10 +8,10 @@
 #define SCL_PORT        GPIOE
 #define SCL_PIN         GPIO_PIN_0 // SCL = D5 = PE0
 
-void 	Soft_I2C_Init(void);
-void 	Soft_I2C_Start(void);
-void 	Soft_I2C_Stop(void);
+void 	  Soft_I2C_Init(void);
+void    Soft_I2C_Start(void);
+void    Soft_I2C_Stop(void);
 uint8_t Soft_I2C_WriteByte(uint8_t byte);
 uint8_t Soft_I2C_ReadByte(uint8_t ack);
 
-#endif /* __INC_SOFT_I2C_H */
+#endif /* __SOFT_I2C_H */
